9a6641d96e001590643b5d4e219dfd80aee0ab3b
Matthias Huber <matthias.huber@iteratec.com>
2022-01-14 12:07:59 +0100

    First draft of Prometheus HTTPS Endpoint. Needs Security Review

--- inspectit-ocelot-config/src/main/java/rocks/inspectit/ocelot/config/model/exporters/metrics/PrometheusExporterSettings.java
+++ inspectit-ocelot-config/src/main/java/rocks/inspectit/ocelot/config/model/exporters/metrics/PrometheusExporterSettings.java
@@ -31,4 +31,31 @@ public class PrometheusExporterSettings {
     @Min(0)
     @Max(65535)
     private int port;
+
+    /**
+     * If true, the inspectIT Agent will start a Prometheus metrics exporter that internally uses an HTTPS Server.
+     *
+     * If true, the properties {{@link #keyStoreType}}, {{@link #keyStoreFile}} and {{@link #keyStorePassword}}
+     * have to be set.
+     */
+    private boolean tlsEnabled;
+
+    /**
+     * Defines the type of the keystore. Either 'JKS' or 'PKCS12'.
+     * If {{@link #useSSL}} is set to 'true', it has to be set!
+     */
+    private String keyStoreType;
+
+    /**
+     * Defines the filepath of the keystore containing the certificate that will be used by the HTTPS Server
+     * If {{@link #useSSL}} is set to 'true', it has to be set!
+     */
+    private String keyStoreFile;
+
+    /**
+     * Defines the password used to access the key in the keystore.
+     * If {{@link #useSSL}} is set to 'true', it has to be set!
+     */
+    // TODO: change to byte[] for security reasons. Describe them
+    private String keyStorePassword;
 }
--- inspectit-ocelot-core/src/main/java/rocks/inspectit/ocelot/core/exporter/PrometheusExporterService.java
+++ inspectit-ocelot-core/src/main/java/rocks/inspectit/ocelot/core/exporter/PrometheusExporterService.java
@@ -1,5 +1,7 @@
 package rocks.inspectit.ocelot.core.exporter;
 
+import com.sun.net.httpserver.HttpsConfigurator;
+import com.sun.net.httpserver.HttpsParameters;
 import io.opencensus.exporter.stats.prometheus.PrometheusStatsCollector;
 import io.opencensus.exporter.stats.prometheus.PrometheusStatsConfiguration;
 import io.prometheus.client.exporter.HTTPServer;
@@ -8,6 +10,16 @@ import lombok.val;
 import org.springframework.stereotype.Component;
 import rocks.inspectit.ocelot.config.model.InspectitConfig;
 import rocks.inspectit.ocelot.core.service.DynamicallyActivatableService;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.SSLParameters;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.security.*;
+import java.security.cert.CertificateException;
 
 import static io.prometheus.client.CollectorRegistry.defaultRegistry;
 
@@ -36,9 +48,20 @@ public class PrometheusExporterService extends DynamicallyActivatableService {
         try {
             String host = config.getHost();
             int port = config.getPort();
-            log.info("Starting Prometheus Exporter on {}:{}", host, port);
+            log.info("Starting Prometheus Exporter on {}://{}:{}", getEndpointScheme(config.isTlsEnabled()), host, port);
             PrometheusStatsCollector.createAndRegister(PrometheusStatsConfiguration.builder().setRegistry(defaultRegistry).build());
-            prometheusClient = new HTTPServer(host, port, true);
+
+            HTTPServer.Builder prometheusClientBuilder = new HTTPServer.Builder()
+                    .withHostname(host)
+                    .withPort(port)
+                    .withDaemonThreads(true);
+
+            if (config.isTlsEnabled()) {
+                HttpsConfigurator  httpsConfigurator = createHttpsConfigurator(config.getKeyStoreType(), config.getKeyStoreFile(), config.getKeyStorePassword());
+                prometheusClientBuilder.withHttpsConfigurator(httpsConfigurator);
+            }
+
+            prometheusClient = prometheusClientBuilder.build();
         } catch (Exception e) {
             log.error("Error Starting Prometheus HTTP Endpoint!", e);
             defaultRegistry.clear();
@@ -51,9 +74,53 @@ public class PrometheusExporterService extends DynamicallyActivatableService {
     protected boolean doDisable() {
         log.info("Stopping Prometheus Exporter");
         if (prometheusClient != null) {
-            prometheusClient.stop();
+            prometheusClient.close();
             defaultRegistry.clear();
         }
         return true;
+    }
+
+    private String getEndpointScheme(boolean tlsEnabled){
+        return tlsEnabled ? "https" : "http";
+    }
+
+    private HttpsConfigurator createHttpsConfigurator(String keystoreType, String keystoreFile, String keystorePassword) throws CertificateException, IOException, NoSuchAlgorithmException, KeyStoreException, UnrecoverableKeyException, KeyManagementException {
+        KeyStore keystore = loadKeyStore(keystoreType, keystoreFile, keystorePassword);
+        KeyManagerFactory keyManagerFactory = createKeyManagerFactory(keystore, keystorePassword);
+        SSLContext sslContext = createSSLContext(keyManagerFactory);
+
+        return new HttpsConfigurator(sslContext) {
+            @Override
+            public void configure(HttpsParameters params) {
+                SSLEngine engine = sslContext.createSSLEngine();
+                params.setNeedClientAuth(false);
+                params.setCipherSuites(engine.getEnabledCipherSuites());
+                params.setProtocols(engine.getEnabledProtocols());
+
+                SSLParameters sslParameters = sslContext.getSupportedSSLParameters();
+                params.setSSLParameters(sslParameters);
+            }
+        };
+    }
+
+    private KeyStore loadKeyStore(String keystoreType, String keystoreFile, String keystorePassword) throws IOException, CertificateException, NoSuchAlgorithmException, KeyStoreException {
+        KeyStore keyStore = KeyStore.getInstance(keystoreType);
+        keyStore.load(new FileInputStream(keystoreFile), keystorePassword.toCharArray());
+
+        return keyStore;
+    }
+
+    private KeyManagerFactory createKeyManagerFactory(KeyStore keyStore, String keystorePassword) throws NoSuchAlgorithmException, UnrecoverableKeyException, KeyStoreException {
+        KeyManagerFactory keyManagerFactory = KeyManagerFactory.getInstance("SunX509");
+        keyManagerFactory.init(keyStore, keystorePassword.toCharArray());
+
+        return keyManagerFactory;
+    }
+
+    private SSLContext createSSLContext(KeyManagerFactory keyManagerFactory) throws NoSuchAlgorithmException, KeyManagementException {
+        SSLContext sslContext = SSLContext.getInstance("TLSv1.2");
+        sslContext.init(keyManagerFactory.getKeyManagers(), null, null);
+
+        return sslContext;
     }
 }
